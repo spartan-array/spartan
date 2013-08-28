8673c79ed7c3dde78e79f67e5dd329e51f3460f0
Russell Power <power@cs.nyu.edu>
2013-08-28 12:38:39 -0400

    Add options for starting via multiprocessing, changing the number of workers.

--- python/tests/test_backend.py
+++ python/tests/test_backend.py
@@ -1,70 +1,72 @@
 from spartan import util
-from spartan.array import prims, compile_expr, backend, distarray, expr
+from spartan.array import prims, compile_expr, backend, distarray, expr, extent
 from spartan.util import Assert
+import math
 import numpy as np
 import test_common
 
-distarray.TILE_SIZE = 10
+DIM = 1000
+distarray.TILE_SIZE = DIM ** 2 / 4
  
-def test_add2(master):
-  a = distarray.ones(master, (10, 10))
-  b = distarray.ones(master, (10, 10))
+def test_add2(ctx):
+  a = distarray.ones(ctx, (DIM, DIM))
+  b = distarray.ones(ctx, (DIM, DIM))
   a = prims.Value(a)
   b = prims.Value(b)
   
   map_prim = prims.MapTiles([a, b], lambda a, b: a + b)
-  c = backend.evaluate(master, map_prim)
+  c = backend.evaluate(ctx, map_prim)
   lc = c.glom()
-  Assert.all_eq(lc, np.ones((10, 10)) * 2)
+  Assert.all_eq(lc, np.ones((DIM, DIM)) * 2)
 
-def test_add3(master):
-  a = distarray.ones(master, (10, 10))
-  b = distarray.ones(master, (10, 10))
-  c = distarray.ones(master, (10, 10))
+def test_add3(ctx):
+  a = distarray.ones(ctx, (DIM, DIM))
+  b = distarray.ones(ctx, (DIM, DIM))
+  c = distarray.ones(ctx, (DIM, DIM))
   
   a = prims.Value(a)
   b = prims.Value(b)
   c = prims.Value(c)
   
   map_prim = prims.MapTiles([a, b, c], lambda a, b, c: a + b + c)
-  d = backend.evaluate(master, map_prim)
+  d = backend.evaluate(ctx, map_prim)
   ld = d.glom()
-  Assert.all_eq(ld, np.ones((10, 10)) * 3)
+  Assert.all_eq(ld, np.ones((DIM, DIM)) * 3)
   
   
-def test_compile_add2(master):
-  a = expr.ones((10, 10))
-  b = expr.ones((10, 10))
-  Assert.all_eq((a + b).evaluate().glom(), np.ones((10, 10)) * 2)
+def test_compile_add2(ctx):
+  a = expr.ones((DIM, DIM))
+  b = expr.ones((DIM, DIM))
+  Assert.all_eq((a + b).evaluate().glom(), np.ones((DIM, DIM)) * 2)
 
 
-def test_compile_add3(master):
-  a = expr.ones((10, 10))
-  b = expr.ones((10, 10))
-  c = expr.ones((10, 10))
-  Assert.all_eq((a + b + c).evaluate().glom(), np.ones((10, 10)) * 3)
+def test_compile_add3(ctx):
+  a = expr.ones((DIM, DIM))
+  b = expr.ones((DIM, DIM))
+  c = expr.ones((DIM, DIM))
+  Assert.all_eq((a + b + c).evaluate().glom(), np.ones((DIM, DIM)) * 3)
 
-def test_compile_add_many(master):
-  a = expr.ones((10, 10))
-  b = expr.ones((10, 10))
-  Assert.all_eq((a + b + a + b + a + b + a + b + a + b).evaluate().glom(), np.ones((10, 10)) * 10)
+def test_compile_add_many(ctx):
+  a = expr.ones((DIM, DIM))
+  b = expr.ones((DIM, DIM))
+  Assert.all_eq((a + b + a + b + a + b + a + b + a + b).evaluate().glom(), np.ones((DIM, DIM)) * 10)
   
 
-def test_sum(master):
-  a = distarray.ones(master, (10, 10))
+def test_sum(ctx):
+  a = distarray.ones(ctx, (DIM, DIM))
   a = prims.Value(a)
   b = prims.Reduce(a, 0, 
                    lambda _: np.float, 
                    lambda ex, tile: np.sum(tile[:], axis=0), 
                    lambda a, b: a + b)
-  c = backend.evaluate(master, b)
+  c = backend.evaluate(ctx, b)
   lc = c.glom()
   Assert.all_eq(lc, np.ones((10,)) * 10)
   
-def test_compile_sum(master):
+def test_compile_sum(ctx):
   def _(axis):
     util.log('Testing sum over axis %s', axis)
-    a = expr.ones((10, 10))
+    a = expr.ones((DIM, DIM))
     b = a.sum(axis=axis)
     val = b.evaluate()
     Assert.all_eq(val.glom(), np.ones((10,10)).sum(axis))
@@ -74,25 +76,44 @@ def test_compile_sum(master):
   _(axis=None)
  
  
-def test_compile_index(master):
-  a = expr.arange((10, 10))
+def test_compile_index(ctx):
+  a = expr.arange((DIM, DIM))
   b = expr.ones((10,))
   z = a[b]  
   val = z.evaluate()
   
-  nx = np.arange(100).reshape(10, 10)
+  nx = np.arange(100).reshape(DIM, DIM)
   ny = np.ones((10,), dtype=np.int)
   
   Assert.all_eq(val.glom(), nx[ny])
   
-def test_slice(master):
-  x = expr.arange((10, 10))
+def test_slice(ctx):
+  x = expr.arange((DIM, DIM))
   z = x[5:8, 5:8]
   zc = compile_expr.compile_op(z)
-  val = backend.evaluate(master, zc)
-  nx = np.arange(100).reshape(10, 10)
+  val = backend.evaluate(ctx, zc)
+  nx = np.arange(100).reshape(DIM, DIM)
   
   Assert.all_eq(val.glom(), nx[5:8, 5:8])
+  
+def test_linear_regression(ctx):
+  N_EXAMPLES = 1000 * 1000 
+  N_DIM = 10
+  x = expr.rand(N_EXAMPLES, N_DIM)
+  y = expr.rand(N_EXAMPLES, 1)
+  w = np.random.rand(N_DIM, 1)
+  
+  for i in range(2):
+    def _dot(inputs, ex):
+      t = inputs[0][ex.to_slice()]
+      out = extent.TileExtent(list(ex.ul[0:1]) + [0],
+                              list(ex.sz[0:1]) + [1],
+                              list(ex.array_shape[0:1]) + [1])
+      return out, t.dot(w)
+    
+    yp = expr.map_extents(x, _dot).evaluate()
+    grad = expr.sum(x * (yp - y), axis=0).glom().reshape((N_DIM, 1))
+    w = w + grad * 0.0001
   
 if __name__ == '__main__':
   test_common.run_cluster_tests(__file__)
--- python/tests/test_common.py
+++ python/tests/test_common.py
@@ -1,24 +1,41 @@
-import spartan
-from spartan import util
+from spartan import util, config
+from spartan.config import flags
 import imp
+import multiprocessing
 import socket
+import spartan
+import sys
+import time
 import types
 
-NUM_WORKERS = 1
+config.add_flag('test_filter', default='')
+config.add_flag('num_workers', default=4, type=int)
+config.add_bool_flag('multiprocess', default=False)
+
+def worker_loop(port): 
+  spartan.start_worker('%s:9999' % socket.gethostname(), port)
+  while 1:
+    time.sleep(1)
+  
 
 def start_cluster():
   spartan.set_log_level(spartan.INFO)
-  master = spartan.start_master(9999, NUM_WORKERS)
-  for i in range(NUM_WORKERS):
-    spartan.start_worker('%s:9999' % socket.gethostname(),  10000 + i)
+  master = spartan.start_master(9999, flags.num_workers)
+  time.sleep(0.1)
+  for i in range(flags.num_workers):
+    if flags.multiprocess:
+      p = multiprocessing.Process(target = lambda: worker_loop(10000 + i))
+      p.daemon = True
+      p.start()
+    else:
+      spartan.start_worker('%s:9999' % socket.gethostname(),  10000 + i)
   return master
 
 def run_cluster_tests(filename):
-  import argparse
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--test_filter', default='')
+  util.log('Loading tests from %s', filename)
+  _, argv = config.parse_known_args(sys.argv)
   
-  flags, argv = parser.parse_known_args()
+  util.log('Rest: %s', argv)
   
   module = imp.load_source('test_module', filename)
   
@@ -34,12 +51,14 @@ def run_cluster_tests(filename):
   util.log('Running tests for module: %s (%s)', module, filename)
   util.log('Tests to run: %s', tests)
   master = start_cluster()
+  
   for testname in tests:
     util.log('Running %s', testname)
     getattr(module, testname)(master)
   
+  if flags.profile_kernels:
+    spartan.api.PROF.dump_stats('kernel_prof.out')
   
-
 
 if __name__ == '__main__':
   raise Exception, 'Should not be run directly.'
