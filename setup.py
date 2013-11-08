496c73e6617aafd0b56c6a481050f557d33d6145
Russell Power <power@cs.nyu.edu>
2013-11-08 18:10:59 -0500

    Add setup files.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, Extension
+
+#from Cython.Build import cythonize
+
+setup(
+  name='spartan',
+  version='0.05',
+  maintainer='Russell Power',
+  maintainer_email='russell.power@gmail.com',
+  url='http://github.com/rjpower/spartan',
+  install_requires=[
+    'appdirs',
+    'numpy>=1.6',
+    'sphinx_bootstrap_theme',                
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Other Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: POSIX',
+    'Programming Language :: C++',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+  ],
+  description='Distributed Numpy-like arrays.',
+  package_dir={'': '.'},
+  packages=['spartan', 
+            'spartan.expr',
+            'spartan.rpc',
+            'spartan.dense',
+            'spartan.sparse'],
+   # + ext_modules = cythonize('tests/netflix.pyx'),
+)
