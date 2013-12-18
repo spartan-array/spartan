be4c70f90addf84e74b544ece0fde5ca966da0da
Russell Power <power@cs.nyu.edu>
2013-12-18 14:08:37 -0500

    Add benchmark for financial operations.

---
+++ tests/benchmark_finance.py
@@ -0,0 +1,17 @@
+from spartan.expr import eager, ones, zeros, glom, force
+from spartan.examples import finance
+import test_common
+
+def benchmark_finance(ctx, timer):
+  current = eager(zeros((1000 * 1000 * 100 * ctx.num_workers,)))
+  strike = eager(ones((1000 * 1000 * 100 * ctx.num_workers,)))
+
+  def _step():
+    put, call = finance.black_scholes(current, strike, 12, 0.05, 0.01)
+    force(call)
+
+  for i in range(3):
+    timer.time_op('black-scholes', _step)
+  
+if __name__ == '__main__':
+  test_common.run(__file__)
