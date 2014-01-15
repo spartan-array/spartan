a4ef4413d585c8b4bd5346993751127e07deda18
Russell Power <power@cs.nyu.edu>
2014-01-15 11:37:53 -0500

    Add test for benchmarking effect of optimizations.

--- tests/benchmark_finance.py
+++ tests/benchmark_finance.py
@@ -1,17 +1,46 @@
-from spartan.expr import eager, ones, zeros, glom, force
+from spartan.expr import eager, ones, zeros, glom, force, randn
 from spartan.examples import finance
+from spartan.config import FLAGS
+
+import numpy as np
 import test_common
 
-def benchmark_finance(ctx, timer):
+def bs_step(current, strike):
+  put, call = finance.black_scholes(current, strike, 12, 0.05, 0.01)
+  force(call)
+ 
+def benchmark_options(ctx, timer):
   current = eager(zeros((10 * 1000 * 1000 * ctx.num_workers,)))
   strike = eager(ones((10 * 1000 * 1000 * ctx.num_workers,)))
-
-  def _step():
-    put, call = finance.black_scholes(current, strike, 12, 0.05, 0.01)
-    force(call)
-
+   
   for i in range(5):
-    timer.time_op('black-scholes', _step)
-  
+    timer.time_op('black-scholes', lambda: bs_step(current, strike))
+   
+def benchmark_jump(ctx, timer): 
+  prices = eager(randn(10 * 1000 * 1000 * ctx.num_workers))
+  def jump_step():
+    changed = finance.find_change(prices, 0.5)
+    force(changed)
+       
+  for i in range(5):
+    timer.time_op('find-change', jump_step)
+
+# def benchmark_optimization(ctx, timer):
+#   current = eager(zeros((100 * 1000 * 1000 * ctx.num_workers,), dtype=np.float32))
+#   strike = eager(ones((100 * 1000 * 1000 * ctx.num_workers,), dtype=np.float32))
+#   
+#   FLAGS.optimization = 0
+#   timer.time_op('opt-none', lambda: bs_step(current, strike))
+#   
+#   FLAGS.optimization = 1
+#   FLAGS.opt_parakeet_gen = 0
+#   FLAGS.opt_map_fusion = 1
+#   timer.time_op('opt-fusion', lambda: bs_step(current, strike))
+#   
+#   FLAGS.opt_parakeet_gen = 1
+#   timer.time_op('opt-parakeet', lambda: bs_step(current, strike))
+#   timer.time_op('opt-parakeet', lambda: bs_step(current, strike))
+ 
+
 if __name__ == '__main__':
   test_common.run(__file__)
