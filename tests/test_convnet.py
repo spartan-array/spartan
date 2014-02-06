276362264e62ce921a14d6b78193579b8e946e85
Russell Power <power@cs.nyu.edu>
2014-02-06 10:40:11 -0500

    Updates to get Travis (mostly) working:
    Patch to fix buggy assumption by multiprocessing library
    Skip tests which require parakeet if it is unavailable.
    Reduce logging for nosetests.
    Use temporary directory instead of hdfs for testing.
    Don't use apt for zmq; package is broken.

--- tests/test_convnet.py
+++ tests/test_convnet.py
@@ -1,5 +1,4 @@
 from spartan import expr, util
-from spartan.expr import stencil
 from test_common import with_ctx
 from math import sqrt
 
@@ -10,33 +9,39 @@ N_IMGS = 1
 N_FILTERS = 1
 ONE_TILE = (10000, 10000, 10000, 10000)
 
-@with_ctx
-def test_convnet(ctx):
-  hint = util.divup(64, sqrt(ctx.num_workers))
+try:
+  import parakeet
+  from spartan.expr import stencil
+
+  @with_ctx
+  def test_convnet(ctx):
+    hint = util.divup(64, sqrt(ctx.num_workers))
+     
+    images = expr.eager(expr.ones((N_IMGS,) + IMG_SIZE,
+                                  tile_hint=(N_IMGS, N_COLORS, hint, hint)))
+    
+    w1 = expr.eager(expr.ones((N_FILTERS, N_COLORS) + FILTER_SIZE,
+                              tile_hint=ONE_TILE))
+    
+    conv1 = stencil.stencil(images, w1, 2)
+    pool1 = stencil.maxpool(conv1)
    
-  images = expr.eager(expr.ones((N_IMGS,) + IMG_SIZE,
-                                tile_hint=(N_IMGS, N_COLORS, hint, hint)))
-  
-  w1 = expr.eager(expr.ones((N_FILTERS, N_COLORS) + FILTER_SIZE,
-                            tile_hint=ONE_TILE))
-  
-  conv1 = stencil.stencil(images, w1, 2)
-  pool1 = stencil.maxpool(conv1)
- 
-  w2 = expr.eager(expr.ones((N_FILTERS, N_FILTERS) + FILTER_SIZE,
-                            tile_hint=ONE_TILE))
-  
-  conv2 = stencil.stencil(pool1, w2, 2)
-  pool2 = stencil.maxpool(conv2)
-  
-  w3 = expr.eager(expr.ones((N_FILTERS, N_FILTERS) + FILTER_SIZE,
-                            tile_hint=ONE_TILE))
-  conv3 = stencil.stencil(pool2, w3, 2)
-  pool3 = stencil.maxpool(conv3)
-  
-  util.log_info(pool3.shape)
-  
-  #raveled3 = expr.reshape(pool3, (N_IMGS, 8 * 8 * 8))
-  #expr.ravel(pool3)
-  #w4 = expr.ones((8 * 8 * 8, N_IMGS))
-  #fc4 = expr.dot(w4, raveled3)
+    w2 = expr.eager(expr.ones((N_FILTERS, N_FILTERS) + FILTER_SIZE,
+                              tile_hint=ONE_TILE))
+    
+    conv2 = stencil.stencil(pool1, w2, 2)
+    pool2 = stencil.maxpool(conv2)
+    
+    w3 = expr.eager(expr.ones((N_FILTERS, N_FILTERS) + FILTER_SIZE,
+                              tile_hint=ONE_TILE))
+    conv3 = stencil.stencil(pool2, w3, 2)
+    pool3 = stencil.maxpool(conv3)
+    
+    util.log_info(pool3.shape)
+    
+    #raveled3 = expr.reshape(pool3, (N_IMGS, 8 * 8 * 8))
+    #expr.ravel(pool3)
+    #w4 = expr.ones((8 * 8 * 8, N_IMGS))
+    #fc4 = expr.dot(w4, raveled3)
+except ImportError:
+  print 'Failed to import parakeet, skipping test.'
--- tests/test_stencil.py
+++ tests/test_stencil.py
@@ -1,6 +1,5 @@
 from spartan import expr, util
 from spartan.array import distarray
-from spartan.expr import stencil
 from spartan.util import Assert, divup
 from test_common import with_ctx
 import math
@@ -10,41 +9,45 @@ import test_common
 import time
 
 
-
-ONE_TILE = (10000, 10000, 10000, 10000)
-
-@with_ctx
-def test_stencil(ctx):
-  st = time.time()
-
-  IMG_SIZE = int(8 * math.sqrt(ctx.num_workers))
-  FILT_SIZE = 8
-  N = 8
-  F = 32
+try:
+  import parakeet
+  from spartan.expr import stencil
   
-  tile_size = util.divup(IMG_SIZE, math.sqrt(ctx.num_workers))
-  
-  images = expr.ones((N, 3, IMG_SIZE, IMG_SIZE), 
-                     dtype=np.float, 
-                     tile_hint=(N, 3, tile_size, tile_size))
-  
-  filters = expr.ones((F, 3, FILT_SIZE, FILT_SIZE), 
-                      dtype=np.float, 
-                      tile_hint=ONE_TILE)
-  
-  result = stencil.stencil(images, filters, 1)
-  ed = time.time()
-  print ed - st
+  ONE_TILE = (10000, 10000, 10000, 10000)
 
-@with_ctx
-def test_local_convolve(ctx):
-  F = 16
-  filters = np.ones((F, 3, 5, 5))
-  for N in [1, 4, 16]:
-    images = np.ones((N, 3, 128, 128))
+  @with_ctx
+  def test_stencil(ctx):
     st = time.time()
-    stencil._convolve(images, filters)
-    print N, F, time.time() - st
 
-if __name__ == '__main__':
-  test_stencil()
+    IMG_SIZE = int(8 * math.sqrt(ctx.num_workers))
+    FILT_SIZE = 8
+    N = 8
+    F = 32
+    
+    tile_size = util.divup(IMG_SIZE, math.sqrt(ctx.num_workers))
+    
+    images = expr.ones((N, 3, IMG_SIZE, IMG_SIZE), 
+                       dtype=np.float, 
+                       tile_hint=(N, 3, tile_size, tile_size))
+    
+    filters = expr.ones((F, 3, FILT_SIZE, FILT_SIZE), 
+                        dtype=np.float, 
+                        tile_hint=ONE_TILE)
+    
+    result = stencil.stencil(images, filters, 1)
+    ed = time.time()
+    print ed - st
+
+  @with_ctx
+  def test_local_convolve(ctx):
+    F = 16
+    filters = np.ones((F, 3, 5, 5))
+    for N in [1, 4, 16]:
+      images = np.ones((N, 3, 128, 128))
+      st = time.time()
+      stencil._convolve(images, filters)
+      print N, F, time.time() - st
+
+except ImportError:
+  pass
+
