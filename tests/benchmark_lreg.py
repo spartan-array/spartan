d479deab1f39e81c3d87f209507e9dee9abe013c
Russell Power <power@cs.nyu.edu>
2014-02-21 17:41:45 -0500

    Update linear regression benchmark.

--- tests/benchmark_lreg.py
+++ tests/benchmark_lreg.py
@@ -6,15 +6,15 @@ import test_common
 
   
 def benchmark_linear_regression(ctx, timer):
-  N_EXAMPLES = 2500000 * ctx.num_workers
+  N_EXAMPLES = 10 * 1000 * 1000 * ctx.num_workers
   N_DIM = 10
   x = expr.rand(N_EXAMPLES, N_DIM, 
-                tile_hint=(N_EXAMPLES / ctx.num_workers, N_DIM))
+                tile_hint=(N_EXAMPLES / ctx.num_workers, N_DIM)).astype(np.float32)
   
   y = expr.rand(N_EXAMPLES, 1, 
-                tile_hint=(N_EXAMPLES / ctx.num_workers, 1))
+                tile_hint=(N_EXAMPLES / ctx.num_workers, 1)).astype(np.float32)
   
-  w = np.random.rand(N_DIM, 1)
+  w = np.random.rand(N_DIM, 1).astype(np.float32)
   
   x = expr.eager(x)
   y = expr.eager(y)
@@ -25,9 +25,10 @@ def benchmark_linear_regression(ctx, timer):
     
     diff = x * (yp - y)
     grad = expr.sum(diff, axis=0).glom().reshape((N_DIM, 1))
-    #w = w - grad * 1e-6
+    wprime = w - grad * 1e-6
+    expr.force(wprime)
 
-  for i in range(5):
+  for i in range(25):
     timer.time_op('linear-regression', _step)
   
 if __name__ == '__main__':
