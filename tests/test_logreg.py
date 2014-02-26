ab39a0da448e4294ec734e4ef0dd22623958235b
Qi Chen <chenqi871025@gmail.com>
2014-02-26 16:58:31 +0000

    add benchmark test for linear_regression, log_regression and svm

--- tests/test_logreg.py
+++ tests/test_logreg.py
@@ -1,4 +1,5 @@
 from spartan.examples import logistic_regression
+from spartan import expr
 import test_common
 
 N_EXAMPLES = 100
@@ -8,3 +9,14 @@ ITERATION = 10
 class TestLogisticRegression(test_common.ClusterTest):
   def test_logreg(self):
     logistic_regression.run(N_EXAMPLES, N_DIM, ITERATION)
+
+def benchmark_logreg(ctx, timer):
+  print "#worker:", ctx.num_workers
+  N_EXAMPLES = 1000000 * ctx.num_workers
+  ITERATION = 100
+  x = expr.eager(expr.rand(N_EXAMPLES, N_DIM, tile_hint=(N_EXAMPLES / ctx.num_workers, N_DIM)))
+  y = expr.eager(expr.rand(N_EXAMPLES, 1, tile_hint=(N_EXAMPLES / ctx.num_workers, 1)))
+  logistic_regression.logistic_regression(x, y, ITERATION)
+  
+if __name__ == '__main__':
+  test_common.run(__file__)
--- tests/test_lreg.py
+++ tests/test_lreg.py
@@ -1,4 +1,5 @@
 from spartan.examples import linear_regression
+from spartan import expr
 import test_common
 
 N_EXAMPLES = 100
@@ -8,3 +9,14 @@ ITERATION = 100
 class TestLinearRegression(test_common.ClusterTest):
   def test_lreg(self):
     linear_regression.run(N_EXAMPLES, N_DIM, ITERATION)
+
+
+def benchmark_lreg(ctx, timer):
+  print "#worker:", ctx.num_workers
+  N_EXAMPLES = 1000000 * ctx.num_workers
+  x = expr.eager(expr.rand(N_EXAMPLES, N_DIM, tile_hint=(N_EXAMPLES / ctx.num_workers, N_DIM)))
+  y = expr.eager(expr.rand(N_EXAMPLES, 1, tile_hint=(N_EXAMPLES / ctx.num_workers, 1)))
+  linear_regression.linear_regression(x, y, ITERATION)
+  
+if __name__ == '__main__':
+  test_common.run(__file__)
