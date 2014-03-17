a06366a3887810df45f5b2b31c106951b714e12f
Chien-Chin Huang <fegins@gmail.com>
2014-03-17 10:38:32 -0400

    Make Broadcast support map over tiles.

---
+++ tests/test_broadcast.py
@@ -0,0 +1,21 @@
+from spartan import expr, util
+from spartan.util import Assert
+import test_common
+import numpy as np
+from scipy import sparse as sp
+from spartan.expr import broadcast
+
+class TestBroadcast(test_common.ClusterTest):
+  def test_broadcast(self):
+    a = expr.ones((100, 1, 100, 100)).force()
+    b = expr.ones((10, 100, 1)).force()
+    a, b = broadcast.broadcast((a, b))
+    c = expr.add(a, b).force()
+    d = expr.sub(a, b).force()
+
+    n = np.ones((100, 10, 100, 100))
+    n1 = n + n
+    n2 = n - n
+    Assert.all_eq(n1, c.glom())
+    Assert.all_eq(n2, d.glom())
+
