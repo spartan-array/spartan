fc40bbb09bc52c1d0b532f025986cc1d79bfeefb
Qi Chen <chenqi871025@gmail.com>
2014-04-16 08:26:46 +0000

    add spectral_clustering application

---
+++ spartan/examples/spectral_clustering.py
@@ -0,0 +1,151 @@
+import numpy as np
+from spartan import expr, util
+from spartan.array import extent
+from spartan.examples import lanczos
+from spartan.examples.sklearn.cluster import KMeans
+
+def manhattan_distance(X, Y):
+  '''
+  Compute the Manhattan distance between point X and Y.
+  
+  Args:
+    X(numpy.ndarray): one point.
+    Y(numpy.ndarray): anther point.
+  '''
+  return np.abs(X - Y).sum()
+
+def euclidean_distance(X, Y, squared=False):
+  '''
+  Compute the Euclidean distance between point X and Y.
+  
+  Args:
+    X(numpy.ndarray): one point.
+    Y(numpy.ndarray): anther point.
+  '''
+  dist = np.square(X - Y).sum()
+  return dist if squared else np.sqrt(dist)
+
+def scaled_euclidean_distance(X, Y):
+  '''
+  calculate scaled Euclidean distance for point X and Y.
+  scale factor c = 2 * (sigma^2), where sigma = pg. 
+  p is a constant of 1.5 that is used in the EigenCuts paper.
+  g is the median of the diff in each dimension of Euclidean distance.
+  
+  Args:
+    X(numpy.ndarray): one point.
+    Y(numpy.ndarray): anther point.
+  '''
+  diff = np.square(X, Y)
+  median = np.median(diff)
+  sum = diff.sum()
+  return 0 if median == 0 else np.exp((-sum) / (((median * 1.5) ** 2) * 2))
+
+def rbf_distance(X, Y, gamma=1.0):
+  '''
+  Compute the rbf (gaussian) kernel between point X and Y.
+  
+  Args:
+    X(numpy.ndarray): one point.
+    Y(numpy.ndarray): anther point.
+  '''
+  K = euclidean_distance(X, Y, squared=True) * -gamma
+  return np.exp(K)
+
+def _row_similarity_mapper(array, ex):
+  '''
+  calculate distances for each pair of points.
+  
+  Args:
+    array(DistArray): the input data points matrix.
+    ex(Extent): region being processed.
+  '''
+  points = array.fetch(ex)
+  result = np.zeros((points.shape[0], array.shape[0]))
+  for other_ex in array.tiles:
+    if ex == other_ex:
+      other_points = points
+    else:
+      other_points = array.fetch(other_ex)
+    
+    for i in range(points.shape[0]):
+      for j in range(other_points.shape[0]):
+        result[i, other_ex.ul[0] + j] = rbf_distance(points[i], other_points[j])
+    
+  yield extent.create((ex.ul[0], 0), (ex.lr[0], array.shape[0]), (array.shape[0], array.shape[0])), result
+
+def _laplacian_mapper(array, ex, D):
+  '''
+  calculate the normalized Laplacian matrix L = D^(-0.5)AD^(-0.5)
+
+  Args:
+    array(DistArray): the adjacency matrix A.
+    ex(Extent): region being processed.
+    D(DistArray): the diagonal matrix which is represented as a 1-dimensional vector.
+  '''
+  A = array.fetch(ex)
+  D_sqrt = np.sqrt(D[:])
+  
+  D_sqrt_zeros = (D_sqrt == 0)
+  D_sqrt[D_sqrt_zeros] = 1
+  D_sqrt = 1.0 / D_sqrt
+  
+  L = np.dot(np.dot(np.diag(D_sqrt[ex.ul[0]:ex.lr[0]]), A), np.diag(D_sqrt[ex.ul[1]:ex.lr[1]]))
+  L[:, ex.ul[0]:ex.lr[0]].flat[::A.shape[0] + 1] = 1
+  yield ex, L
+  
+def spectral_cluster(points, k=10, num_iter=10):
+  '''
+  clustering data points using kmeans spectral clustering method.
+
+  Args:
+    points(Expr or DistArray): the data points to be clustered.
+    k(int): the number of clusters we need to generate.
+    num_iter(int): the max number of iterations that kmeans clustering method runs. 
+  '''  
+  # calculate similarity for each pair of points to generate the adjacency matrix A
+  A = expr.shuffle(points, _row_similarity_mapper)
+  
+  num_dims = A.shape[1]
+  
+  # Construct the diagonal matrix D
+  D = expr.sum(A, axis=1, tile_hint=(A.shape[0],)).force()
+  
+  # Calculate the normalized Laplacian of the form: L = D^(-0.5)AD^(-0.5)
+  L = expr.shuffle(A, _laplacian_mapper, kw={'D': D})
+  
+  # Perform eigen-decomposition using Lanczos solver
+  overshoot = min(k * 2, num_dims) 
+  d, U = lanczos.solve(L, L, overshoot, True)
+  U = U[:, 0:k]
+  
+  # Generate initial clusters which picks rows as centers if that row contains max eigen 
+  # value in that column
+  init_clusters = U[np.argmax(U, axis=0)]
+  
+  # Run kmeans clustering with init_clusters
+  kmeans = KMeans(k, num_iter)
+  U = expr.from_numpy(U, tile_hint=None).force()
+  centers, labels = kmeans.fit(U, init_clusters)
+  
+  return labels
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
---
+++ tests/test_spectral_clustering.py
@@ -0,0 +1,22 @@
+import test_common
+from test_common import millis
+from spartan.examples.spectral_clustering import spectral_cluster
+from spartan import expr
+from datetime import datetime    
+
+def benchmark_spectral_clustering(ctx, timer):
+  N_PTS = 100 * ctx.num_workers
+  N_DIM = 2
+  ITER = 100
+  N_CENTERS = 5
+
+  pts = expr.rand(N_PTS, N_DIM,
+                  tile_hint=(N_PTS / ctx.num_workers, N_DIM)).force()
+
+  t1 = datetime.now()
+  cluster_result = spectral_cluster(pts, N_CENTERS, ITER)
+  t2 = datetime.now()
+  print 'spectral_cluster time:%s ms' % millis(t1, t2)
+  
+if __name__ == '__main__':
+  test_common.run(__file__)
