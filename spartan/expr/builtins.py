Fix stop calculation and add arange start tests

spartan/expr/builtins.py
@@ -343,14 +343,14 @@ def arange1d(start, stop, step=1, dtype=np.float, tile_hint=None):
                     kw={'start': start, 'step':step})
 
 
-def _arange_mapper(inputs, ex, dtype=None):
+def _arange_mapper(inputs, ex, start, stop, step, dtype=None):
   pos = extent.ravelled_pos(ex.ul, ex.array_shape)
   #util.log_info('Extent: %s, shape:%s, pos: %s', ex, ex.shape, pos)
   sz = np.prod(ex.shape)
   yield (ex, np.arange(pos, pos + sz, dtype=dtype).reshape(ex.shape))
 
 
-def arange(shape, dtype=np.float, tile_hint=None):
+def arange(shape, start=0, stop=None, step=1, dtype=np.float, tile_hint=None):
   '''
   An extended version of `np.arange`.  
   
@@ -362,9 +362,13 @@ def arange(shape, dtype=np.float, tile_hint=None):
   :param tile_hint:
   :rtype: `Expr`
   '''
+  if stop is None:
+    stop = step*(np.prod(shape) + start)
+    #util.log_info('stop: %d', stop)
+
   return shuffle(ndarray(shape, dtype=dtype, tile_hint=tile_hint),
-                 fn=_arange_mapper,
-                 kw={'dtype': dtype})
+                 fn=_arange_mapper, kw={'start': start, 'stop': stop,
+                                        'step': step, 'dtype': dtype})
 
 
 def _sum_local(ex, data, axis):

tests/test_builtins.py
@@ -4,17 +4,29 @@ import spartan
 import numpy as np
 import test_common
 from spartan.util import Assert
+from spartan.util import log_info
 
 class BuiltinTest(test_common.ClusterTest):
-  def test_arange1d(self):
-    Assert.all_eq(spartan.arange1d(1, 10).force().glom(), np.arange(1, 10))
-    Assert.all_eq(spartan.arange1d(0, 10, 2).force().glom(), np.arange(0, 10, 2))
-    Assert.all_eq(spartan.arange1d(0, 1, 2).force().glom(), np.arange(0, 1, 2))
+  def test_arange(self):
+    # Arange with shape
+    Assert.all_eq(spartan.arange((10, )).force().glom(), np.arange(10))
+    Assert.all_eq(spartan.arange((3, 5)).force().glom(), np.arange(15).reshape((3, 5)))
+
+    # Arange with shape, start
+    Assert.all_eq(spartan.arange((10, ), start=-1).force().glom(), np.arange(-1, 9))
+    Assert.all_eq(spartan.arange((10, ), start=1).force().glom(), np.arange(1, 11))
+
+    # Arange with shape, start, stop
+
+    # Arange with shape, start, stop, step
+    #Assert.all_eq(spartan.arange((10, ), 
+    #Assert.all_eq(spartan.arange(0, 10, 2).force().glom(), np.arange(0, 10, 2))
+    #Assert.all_eq(spartan.arange(0, 1, 2).force().glom(), np.arange(0, 1, 2))
 
     # These tests (and functionality) should be added to give expected
     # NumPy semantics.
-    #Assert.all_eq(spartan.arange1d(-1, 2).force().glom(), np.arange(-1,2))
-    #Assert.all_eq(spartan.arange1d(0, 0).force().glom(), np.arange(0, 0))
+    #Assert.all_eq(spartan.arange(-1, 2).force().glom(), np.arange(-1,2))
+    #Assert.all_eq(spartan.arange(0, 0).force().glom(), np.arange(0, 0))
 
   def _test_bincount(self):
     src = np.asarray([1, 1, 1, 2, 2, 5, 5, 10])