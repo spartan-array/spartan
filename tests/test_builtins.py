2e319a8e3c8edb746815367de85878299aa20c30
Robert Gardner <rhg259@nyu.edu>
2014-07-17 09:25:38 +0000

    Add basic arange1d and tests

--- tests/test_builtins.py
+++ tests/test_builtins.py
@@ -6,19 +6,31 @@ import test_common
 from spartan.util import Assert
 
 class BuiltinTest(test_common.ClusterTest):
-  def test_bincount(self):
+  def test_arange1d(self):
+    Assert.all_eq(spartan.arange1d(1, 10).force().glom(), np.arange(1, 10))
+    Assert.all_eq(spartan.arange1d(0, 10, 2).force().glom(), np.arange(0, 10, 2))
+    Assert.all_eq(spartan.arange1d(0, 1, 2).force().glom(), np.arange(0, 1, 2))
+
+    # These tests (and functionality) should be added to give expected
+    # NumPy semantics.
+    #Assert.all_eq(spartan.arange1d(-1).glom(), np.arange(-1))
+    #Assert.all_eq(spartan.arange1d(0).glom(), np.arange(0))
+    #Assert.all_eq(spartan.arange1d(1).glom(), np.arange(1))
+    #Assert.all_eq(spartan.arange1d(10), np.arange(10))
+
+  def _test_bincount(self):
     src = np.asarray([1, 1, 1, 2, 2, 5, 5, 10])
     Assert.all_eq(
         spartan.bincount(spartan.from_numpy(src)).glom(),
         np.bincount(src))
 
-  def test_max(self):
+  def _test_max(self):
     src = np.asarray([1, 1, 1, 2, 2, 5, 5, 10])
     Assert.all_eq(
         spartan.max(spartan.from_numpy(src)).glom(),
         np.max(src))
-  
-  def test_min(self):
+
+  def _test_min(self):
     src = np.asarray([1, 1, 1, 2, 2, 5, 5, 10])
     Assert.all_eq(
         spartan.min(spartan.from_numpy(src)).glom(),
