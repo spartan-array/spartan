c659acd3d1fb134cb48042056b6012f030c197f9
Robert Gardner <rhg259@nyu.edu>
2014-07-17 11:59:31 +0000

    Refactor arange tests

--- tests/test_builtins.py
+++ tests/test_builtins.py
@@ -4,42 +4,53 @@ import spartan
 import numpy as np
 import test_common
 from spartan.util import Assert
-from spartan.util import log_info
 
 class BuiltinTest(test_common.ClusterTest):
   def test_arange(self):
     # Arange with shape
-    Assert.all_eq(spartan.arange((10, )).force().glom(), np.arange(10))
-    Assert.all_eq(spartan.arange((3, 5)).force().glom(), np.arange(15).reshape((3, 5)))
+    Assert.all_eq(spartan.arange((10, )).glom(), np.arange(10))
+    Assert.all_eq(spartan.arange((3, 5)).glom(), np.arange(15).reshape((3, 5)))
 
     # Arange with shape, start
-    Assert.all_eq(spartan.arange((10, ), start=-1).force().glom(), np.arange(-1, 9))
-    Assert.all_eq(spartan.arange((10, ), start=1).force().glom(), np.arange(1, 11))
-    Assert.all_eq(spartan.arange((3, 5), start=-1).force().glom(), np.arange(-1, 14).reshape((3, 5)))
+    Assert.all_eq(spartan.arange((10, ), start=-1).glom(), np.arange(-1, 9))
+    Assert.all_eq(spartan.arange((10, ), start=1).glom(), np.arange(1, 11))
+    Assert.all_eq(
+        spartan.arange((3, 5), start=-1).glom(),
+        np.arange(-1, 14).reshape((3, 5)))
 
     # Arange with shape, step
-    Assert.all_eq(spartan.arange((10, ), step=2).force().glom(), np.arange(0, 20, 2))
-    Assert.all_eq(spartan.arange((3, 5), step=2).force().glom(), np.arange(0, 30, 2).reshape((3, 5)))
+    Assert.all_eq(spartan.arange((10, ), step=2).glom(), np.arange(0, 20, 2))
+    Assert.all_eq(
+        spartan.arange((3, 5), step=2).glom(),
+        np.arange(0, 30, 2).reshape((3, 5)))
 
     # Arange with shape, start, step
-    Assert.all_eq(spartan.arange((10, ), start=-1, step=2).force().glom(), np.arange(-1, 19, 2))
-    Assert.all_eq(spartan.arange((10, ), start=1, step=2).force().glom(), np.arange(1, 21, 2))
-    Assert.all_eq(spartan.arange((3, 5), start=1, step=2).force().glom(), np.arange(1, 31, 2).reshape((3, 5)))
+    Assert.all_eq(
+        spartan.arange((10, ), start=-1, step=2).glom(),
+        np.arange(-1, 19, 2))
+
+    Assert.all_eq(
+        spartan.arange((10, ), start=1, step=2).glom(),
+        np.arange(1, 21, 2))
+
+    Assert.all_eq(
+        spartan.arange((3, 5), start=1, step=2).glom(),
+        np.arange(1, 31, 2).reshape((3, 5)))
 
 
-  def _test_bincount(self):
+  def test_bincount(self):
     src = np.asarray([1, 1, 1, 2, 2, 5, 5, 10])
     Assert.all_eq(
         spartan.bincount(spartan.from_numpy(src)).glom(),
         np.bincount(src))
 
-  def _test_max(self):
+  def test_max(self):
     src = np.asarray([1, 1, 1, 2, 2, 5, 5, 10])
     Assert.all_eq(
         spartan.max(spartan.from_numpy(src)).glom(),
         np.max(src))
 
-  def _test_min(self):
+  def test_min(self):
     src = np.asarray([1, 1, 1, 2, 2, 5, 5, 10])
     Assert.all_eq(
         spartan.min(spartan.from_numpy(src)).glom(),
