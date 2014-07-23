675ca7ace74d6fab63caee1cadccf33dd9706ff4
Robert Gardner <rhg259@nyu.edu>
2014-07-23 08:40:04 +0000

    Replace explicit with random in maximum tests.

--- tests/test_elementwise.py
+++ tests/test_elementwise.py
@@ -8,20 +8,18 @@ from spartan.util import Assert
 class BuiltinTest(test_common.ClusterTest):
   def test_maximum(self):
     # Test arrays of equal length.
-    np_a = np.array([2, 3, 4])
-    np_b = np.array([1, 5, 2])
-    sp_a = spartan.expr.from_numpy(np_a)
-    sp_b = spartan.expr.from_numpy(np_b)
+    np_a = np.random.randn(10, 10)
+    np_b = np.random.randn(10, 10)
+    sp_a = spartan.from_numpy(np_a)
+    sp_b = spartan.from_numpy(np_b)
     Assert.all_eq(
         spartan.maximum(sp_a, sp_b).glom(),
         np.maximum(np_a, np_b))
 
     # Test broadcasting.
-    np_c = np.array([1, -1, 3, -5])
-    sp_c = spartan.expr.from_numpy(np_c)
     Assert.all_eq(
-        spartan.maximum(sp_c, 0).glom(),
-        np.maximum(np_c, 0))
+        spartan.maximum(sp_a, 0).glom(),
+        np.maximum(np_a, 0))
 
 
 if __name__ == '__main__':
