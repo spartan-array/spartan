26806927a450978bda30ae00b46da86b0e79da66
Robert Gardner <rhg259@nyu.edu>
2014-07-28 09:09:36 +0000

    Finish changing nbody code for spartan
    Add `set_diagonal` function.

--- spartan/examples/nbody.py
+++ spartan/examples/nbody.py
@@ -4,17 +4,56 @@ project.
 bohrium/bridge/bhpy/examples/nbody.py
 
 This simulation uses spartan for `diagonal`, `randn`, `sqrt`, `sum`, and
-`zeros` function calls. Only `randn` required modification because it did not
+`zeros` function calls. Only `randn` required modification because it does not
 have the dtype API that numpy supports (it returns np.float).
 '''
 
 import numpy as np
+import spartan
 from spartan import diagonal, rand, sqrt, sum, transpose, zeros
 
 G = 6.67384e-11       # m/(kg*(s^2))
 dt = 60*60*24*365.25  # Years in seconds
 r_ly = 9.4607e15      # Lightyear in m
 m_sol = 1.9891e30     # Solar mass in kg
+
+
+def _set_diagonal_mapper(array, ex, diag_data):
+  '''Replaces values along the diagonal with the values in data.
+
+  Every mapper function has `array`, the DistArray, and `ex`, the extent
+  this worker has been assigned. To fetch this extent from array, use
+  `data = array.fetch(ex)`. The worker does not have to fetch this extent; you
+  can construct your own using `extent.create(ul, lr, array.shape)`
+
+  '''
+  if ex.ul[0] >= ex.ul[1] and ex.ul[0] < ex.lr[1]:  # Below the diagonal.
+    above, below = False, True
+  elif ex.ul[1] >= ex.ul[0] and ex.ul[1] < ex.lr[0]:  # Above the diagonal.
+    above, below = True, False
+  else:  # Not on the diagonal.
+    yield (ex, array.fetch(ex))
+
+  start = ex.ul[above]
+  stop = min(ex.lr[above], ex.lr[below])
+
+  data = array.fetch(ex)
+  index = 0
+  for i in range(start, stop):
+    data[i - ex.ul[0], i - ex.ul[1]] = diag_data[i - ex.ul[0]]
+    index += 1
+
+  yield (ex, data)
+
+
+def set_diagonal(array, data):
+  '''Creates a copy of array with elements from data on the diagonal.
+
+  Spartan does not support views because data is not contiguous - so all
+  expressions are read-only.
+
+  '''
+  return spartan.shuffle(array, _set_diagonal_mapper, kw={'diag_data': data})
 
 
 def random_galaxy(n):
@@ -44,27 +83,29 @@ def move(galaxy, dt):
 
   # Euclidian distances (all bodys).
   r = sqrt(dx**2 + dy**2 + dz**2)
-  diagonal(r)[:] = 1.0
+  set_diagonal(r, 1.0)
 
   # Prevent collision.
   mask = r < 1.0
-  r = r * ~mask + 1.0 * mask
+  #r = r * ~mask + 1.0 * mask
+  r = spartan.map((r, mask), lambda x, m: x * ~m + 1.0 * m)
 
   m = transpose(galaxy['m'][np.newaxis,:])
 
   # Calculate the acceleration component wise.
-  Fx = G*m*dx / r**3
-  Fy = G*m*dy / r**3
-  Fz = G*m*dz / r**3
+  r_cubed = r**3
+  Fx = G*m*dx / r_cubed
+  Fy = G*m*dy / r_cubed
+  Fz = G*m*dz / r_cubed
 
   # Set the force (acceleration) a body exerts on itself to zero.
-  diagonal(Fx)[:] = 0.0
-  diagonal(Fy)[:] = 0.0
-  diagonal(Fz)[:] = 0.0
+  set_diagonal(Fx, 0.0)
+  set_diagonal(Fy, 0.0)
+  set_diagonal(Fz, 0.0)
 
-  galaxy['vx'] += dt*sp.sum(Fx, axis=0)
-  galaxy['vy'] += dt*sp.sum(Fy, axis=0)
-  galaxy['vz'] += dt*sp.sum(Fz, axis-0)
+  galaxy['vx'] += dt*sum(Fx, axis=0)
+  galaxy['vy'] += dt*sum(Fy, axis=0)
+  galaxy['vz'] += dt*sum(Fz, axis=0)
 
   galaxy['x'] += dt*galaxy['vx']
   galaxy['y'] += dt*galaxy['vy']
--- tests/benchmark_nbody.py
+++ tests/benchmark_nbody.py
@@ -1,7 +1,8 @@
 
-import spartan as sp
+import spartan
 import test_common
 
+from spartan import util
 from spartan.examples import nbody
 
 NUM_BODIES = 100000
@@ -10,7 +11,10 @@ TIMESTEPS = 100000
 
 def benchmark_nbody(ctx, timer):
   galaxy = nbody.random_galaxy(NUM_BODIES)
+  util.log_info('galaxy: %s', galaxy)
+
   nbody.simulate(galaxy, TIMESTEPS)
+
   r = sp.sum(galaxy['x'] + galaxy['y'] + galaxy['z'])
   util.log_info('r: %s', r.glom())
 
