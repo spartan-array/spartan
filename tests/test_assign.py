5f91857fb1c3836477a751a4817eba70dba0612d
Robert Gardner <rhg259@nyu.edu>
2014-07-29 12:07:58 +0000

    Add DistArray support to assign

--- tests/test_assign.py
+++ tests/test_assign.py
@@ -3,23 +3,28 @@
 import spartan
 import numpy as np
 import test_common
-from spartan.expr import from_numpy
+
+from spartan.expr import assign, from_numpy
 from spartan.util import Assert
 
 class BuiltinTest(test_common.ClusterTest):
   def test_assign_array_like(self):
-    # Test list
-    # Test np.ndarray
-    pass
+    a = np.zeros((20, 10))
+    b = np.ones((10, ))
+    region = (10, slice(None))  # same as [10, :]
+
+    sp_a = assign(from_numpy(a), region, b)
+    a[region] = b
+    Assert.all_eq(sp_a.glom(), a)
 
 
   def test_assign_expr(self):
     a = np.zeros((20, 10))
     b = np.ones((10, ))
+    region = (10, slice(None))
 
-    a_slice = (10, slice(None))  # Same as [10, :]
-    sp_a = spartan.assign(from_numpy(a), a_slice, b)
-    a[a_slice] = b
+    sp_a = assign(from_numpy(a), region, from_numpy(b))
+    a[region] = b
     Assert.all_eq(sp_a.glom(), a)
 
 
