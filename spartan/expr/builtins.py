732c0e0ced65f8d0998718dd472f4d92e32a488b
Qi Chen <chenqi871025@gmail.com>
2014-08-19 13:04:46 +0000

    fix scan shape error

--- spartan/expr/builtins.py
+++ spartan/expr/builtins.py
@@ -469,58 +469,44 @@ def min(x, axis=None, tile_hint=None):
       tile_hint = tile_hint)
 
 
-def _scan_reduce_mapper(array, ex, reduce_fn=None, axis=None):
-  if reduce_fn is None:
-    yield (ex, array.fetch(ex))
-  else:  
-    local_reduction = reduce_fn(array.fetch(ex), axis=axis)
-    if axis is None:
-      exts = sorted(array.tiles.keys(), key=lambda x: x.ul)
-      id = exts.index(ex)
-      dst_ex = extent.create((id,),(id+1,),(len(exts),))
-    else:
-      max_axis_shape = __builtin__.max([ext.shape[axis] for ext in array.tiles.keys()])
-      id = ex.ul[axis] / max_axis_shape
-      new_ul = list(ex.ul)
-      new_lr = list(ex.lr)
-      new_shape = list(ex.array_shape)
-      new_ul[axis] = id
-      new_lr[axis] = id + 1
-      new_shape[axis] = int(np.ceil(array.shape[axis] * 1.0 / max_axis_shape))
-    
-      dst_ex = extent.create(new_ul, new_lr, new_shape)
-      
-    local_reduction = np.asarray(local_reduction).reshape(dst_ex.shape)
-    #util.log_info('2 orig_ex:%s dst_ex:%s local_reduction:%s shape:%s', ex, dst_ex, local_reduction, local_reduction.shape)
-    yield (dst_ex, local_reduction)
+def _scan_reduce_mapper(array, ex, reduce_fn, axis):
+  local_reduction = reduce_fn(array.fetch(ex), axis=axis)
+  axis_shape = array.tile_shape()[axis]
+  id = (ex.lr[axis]-1) / axis_shape
+  new_ul = list(ex.ul)
+  new_lr = list(ex.lr)
+  new_shape = list(ex.array_shape)
+  new_ul[axis] = id
+  new_lr[axis] = id + 1
+  new_shape[axis] = int(np.ceil(array.shape[axis] * 1.0 / axis_shape))
 
-def _scan_mapper(array, ex, scan_fn=None, axis=None, scan_base=None):
-  if scan_fn is None:
-    yield (ex, array.fetch(ex))
+  dst_ex = extent.create(new_ul, new_lr, new_shape)
+
+  local_reduction = np.asarray(local_reduction).reshape(dst_ex.shape)
+  yield (dst_ex, local_reduction)
+
+def _scan_mapper(array, ex, scan_fn, axis=None, scan_base=None):
+  local_data = array.fetch(ex)
+  if sp.issparse(local_data): local_data = local_data.todense()
+  
+  if axis is None:
+    axis = 1
+    id = (ex.lr[axis] - 1) / array.tile_shape()[axis]
+    base_slice = list(ex.to_slice())
+    base_slice[axis] = slice(id, id+1, None)
+    new_slice = [slice(0, ex.shape[i], None) for i in range(len(ex.shape))]
+    new_slice[axis] = slice(0,1,None)
+    local_data[new_slice] += scan_base[base_slice]
   else:
-    local_data = array.fetch(ex)
-    if sp.issparse(local_data): local_data = local_data.todense()
-    
-    if axis is None:
-      axis = 1
-      id = ex.ul[axis] / array.tile_shape()[axis]
+    id = (ex.lr[axis] - 1) / array.tile_shape()[axis]
+    if id > 0:
       base_slice = list(ex.to_slice())
-      base_slice[axis] = slice(id, id+1, None)
+      base_slice[axis] = slice(id-1, id, None)
       new_slice = [slice(0, ex.shape[i], None) for i in range(len(ex.shape))]
       new_slice[axis] = slice(0,1,None)
       local_data[new_slice] += scan_base[base_slice]
-    else:
-      id = ex.ul[axis] / array.tile_shape()[axis]
-      if id > 0:
-        base_slice = list(ex.to_slice())
-        base_slice[axis] = slice(id-1, id, None)
-        new_slice = [slice(0, ex.shape[i], None) for i in range(len(ex.shape))]
-        new_slice[axis] = slice(0,1,None)
-        local_data[new_slice] += scan_base[base_slice]
-    
-    #util.log_info('local_data type:%s data:%s', type(local_data), local_data)
-        
-    yield (ex, np.asarray(scan_fn(local_data, axis=axis)).reshape(ex.shape))   
+      
+  yield (ex, np.asarray(scan_fn(local_data, axis=axis)).reshape(ex.shape))   
       
 def scan(array, reduce_fn=np.sum, scan_fn=np.cumsum, axis=None):
   '''
