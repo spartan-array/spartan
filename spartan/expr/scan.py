da9109cde4b23c2f8253dfe8986e20ff1ae5000c
Robert Gardner <rhg259@nyu.edu>
2014-08-20 13:29:57 +0000

    Reimplement scan with map_with_location.

--- spartan/expr/scan.py
+++ spartan/expr/scan.py
@@ -1,7 +1,14 @@
 import numpy as np
 import scipy.sparse as sp
 from ..array import extent
+from .map_with_location import map_with_location
 from .shuffle import shuffle
+
+
+def ex_to_slice(tup):
+  '''Converts an extent (represented as a tuple) to a slice.'''
+  return tuple([slice(ul, lr) for ul, lr in zip(tup[0], tup[1])])
+
 
 def _scan_reduce_mapper(array, ex, reduce_fn, axis):
   local_reduction = reduce_fn(array.fetch(ex), axis=axis)
@@ -19,47 +26,58 @@ def _scan_reduce_mapper(array, ex, reduce_fn, axis):
   local_reduction = np.asarray(local_reduction).reshape(dst_ex.shape)
   yield (dst_ex, local_reduction)
 
-def _scan_mapper(array, ex, scan_fn, axis=None, scan_base=None):
-  local_data = array.fetch(ex)
-  if sp.issparse(local_data): local_data = local_data.todense()
-  
+
+def _scan_mapper(tile, ex, scan_fn=None, axis=None, scan_base=None, tile_shape=None):
+  if sp.issparse(tile):
+    tile = tile.todense()
+
   if axis is None:
     axis = 1
-    id = (ex.lr[axis] - 1) / array.tile_shape()[axis]
-    base_slice = list(ex.to_slice())
-    base_slice[axis] = slice(id, id+1, None)
-    new_slice = [slice(0, ex.shape[i], None) for i in range(len(ex.shape))]
-    new_slice[axis] = slice(0,1,None)
-    local_data[new_slice] += scan_base[base_slice]
+    id = (ex[1][axis] - 1) / tile_shape[axis]
+    base_slice = list(ex_to_slice(ex))
+    base_slice[axis] = slice(id, id+1)
+    new_slice = [slice(0, length) for length in tile.shape]
+    new_slice[axis] = slice(0, 1)
+    tile[new_slice] += scan_base[base_slice]
   else:
-    id = (ex.lr[axis] - 1) / array.tile_shape()[axis]
+    id = (ex[1][axis] - 1) / tile_shape[axis]
     if id > 0:
-      base_slice = list(ex.to_slice())
-      base_slice[axis] = slice(id-1, id, None)
-      new_slice = [slice(0, ex.shape[i], None) for i in range(len(ex.shape))]
-      new_slice[axis] = slice(0,1,None)
-      local_data[new_slice] += scan_base[base_slice]
-      
-  yield (ex, np.asarray(scan_fn(local_data, axis=axis)).reshape(ex.shape))   
-      
+      base_slice = list(ex_to_slice(ex))
+      base_slice[axis] = slice(id-1, id)
+      new_slice = [slice(0, length) for length in tile.shape]
+      new_slice[axis] = slice(0, 1)
+      tile[new_slice] += scan_base[base_slice]
+
+  return np.asarray(scan_fn(tile, axis=axis)).reshape(tile.shape)
+
+
 def scan(array, reduce_fn=np.sum, scan_fn=np.cumsum, axis=None):
   '''
   Scan ``array`` over ``axis``.
-  
-  
-  :param array: The array to scan.
-  :param reduce_fn: local reduce function
-  :param scan_fn: scan function
-  :param axis: Either an integer or ``None``.
+
+  :param array: Expr
+    The array to scan.
+  :param reduce_fn: function, optional
+    Local reduce function; defaults to ``np.sum``.
+  :param scan_fn: function, optional
+    The scan function; defaults to ``np.cumsum``.
+  :param axis: int, optional
+    The axis to scan; default is flattened matrix.
+
+  :rtype: MapExpr
+
   '''
-  reduce_result = shuffle(array, fn=_scan_reduce_mapper, kw={'axis': axis if axis is not None else 1,
-                                                             'reduce_fn': reduce_fn}, shape_hint=array.shape)
+  reduce_result = shuffle(array, fn=_scan_reduce_mapper,
+                          kw={'axis': axis if axis is not None else 1,
+                              'reduce_fn': reduce_fn}, shape_hint=array.shape)
   fetch_result = reduce_result.optimized().glom()
   if axis is None:
     fetch_result = np.concatenate((np.zeros(1), scan_fn(fetch_result, axis=None)[:-1])).reshape(fetch_result.shape)
   else:
     fetch_result = scan_fn(fetch_result, axis=axis)
-  scan_result = shuffle(array, fn=_scan_mapper, kw={'scan_fn':scan_fn,
-                                                    'axis': axis,
-                                                    'scan_base':fetch_result})
-  return scan_result
+
+  return map_with_location(array, _scan_mapper,
+                           fn_kw={'scan_fn': scan_fn,
+                                  'axis': axis,
+                                  'scan_base': fetch_result,
+                                  'tile_shape': array.force().tile_shape()})
