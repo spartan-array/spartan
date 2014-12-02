24cc8454843c17f78920b7596d7d6978a1fc3b65
Hung-Ting Wen <htw230@nyu.edu>
2014-12-02 17:03:02 +0000

    Fix bugs to input dimension constraint

--- spartan/expr/builtins.py
+++ spartan/expr/builtins.py
@@ -171,6 +171,7 @@ def _diagflat_mapper(array, ex):
   :param ex: Extent
     Region being processed.
   '''
+  '''
   util.log_info('ex.ul: %s ex.lr: %s ex.shape: %s', ex.ul, ex.lr, ex.shape)
   util.log_info('array.shape: %s', array.shape)
 
@@ -183,7 +184,29 @@ def _diagflat_mapper(array, ex):
   data = np.ravel(array.fetch(ex))
 
   util.log_info('data: %s %s', data, data.shape)
+  util.log_info('zeros: %s %s', new_lr[0] - new_ul[0], new_lr[1] - new_ul[1])
+  result = np.zeros((new_lr[0] - new_ul[0], new_lr[1] - new_ul[1]))
+  for i in xrange(data.shape[0]):
+    result[i, new_ul[0] + i] = data[i]
 
+  '''
+  #util.log_info('ex.ul: %s ex.lr: %s ex.shape: %s', ex.ul, ex.lr, ex.shape)
+  #util.log_info('array.shape: %s', array.shape)
+
+  head = extent.ravelled_pos(ex.ul, array.shape)
+  tail = extent.ravelled_pos([l-1 for l in ex.lr], array.shape)
+  numElements = np.prod(array.shape)
+
+  new_ul = (head, 0)
+  new_lr = (tail+1, numElements)
+  new_shape = (numElements, numElements)
+  new_ex = extent.create(new_ul, new_lr, new_shape)
+
+  #util.log_info('new_ul: %s new_lr: %s new_shape: %s', new_ul, new_lr, new_shape)
+  data = np.ravel(array.fetch(ex))
+  
+  #util.log_info('data: %s %s', data, data.shape)
+  #util.log_info('zeros: %s %s', new_lr[0] - new_ul[0], new_lr[1] - new_ul[1])
   result = np.zeros((new_lr[0] - new_ul[0], new_lr[1] - new_ul[1]))
   for i in xrange(data.shape[0]):
     result[i, new_ul[0] + i] = data[i]
@@ -198,7 +221,6 @@ def diagflat(array):
   :param array: 2D DistArray
     The data to fill the diagonal.
   '''
-  Assert.eq(len(array.shape), 2)
   return shuffle(array, _diagflat_mapper, shape_hint=(np.prod(array.shape), np.prod(array.shape)))
 
 
