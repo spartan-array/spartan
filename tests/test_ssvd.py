ec615dc88b67d83607c6721686af94b53ffcfbd7
Chien-Chin Huang <fegins@gmail.com>
2015-01-28 21:18:12 -0500

    Add set_random_seed() for those applications which need all works are truely random.

--- tests/test_ssvd.py
+++ tests/test_ssvd.py
@@ -1,5 +1,5 @@
 import test_common
-from spartan.examples.ssvd import svd 
+from spartan.examples.ssvd import svd
 from spartan import expr, util, blob_ctx
 import numpy as np
 from numpy import absolute, linalg
@@ -10,19 +10,21 @@ DIM = (120, 30)
 
 class TestSSVD(test_common.ClusterTest):
   def test_ssvd(self):
-    ctx = blob_ctx.get() 
+    expr.set_random_seed()
+    ctx = blob_ctx.get()
     # Create a sparse matrix.
-    A = expr.randn(*DIM, tile_hint = (int(DIM[0]/ctx.num_workers), DIM[1]), 
+    A = expr.randn(*DIM, tile_hint = (int(DIM[0]/ctx.num_workers), DIM[1]),
                    dtype=np.float64)
-    
+
     U,S,VT = svd(A)
     U2,S2,VT2 = linalg.svd(A.glom(), full_matrices=0)
-    
+
     assert np.allclose(absolute(U.glom()), absolute(U2))
     assert np.allclose(absolute(S), absolute(S2))
     assert np.allclose(absolute(VT), absolute(VT2))
 
 def benchmark_ssvd(ctx, timer):
+  expr.set_random_seed()
   DIM = (1280, 1280)
   #A = expr.randn(*DIM, dtype=np.float64)
   A = np.random.randn(*DIM)
@@ -31,7 +33,7 @@ def benchmark_ssvd(ctx, timer):
   U,S,VT = svd(A)
   t2 = datetime.now()
   cost_time = millis(t1, t2)
-    
+
   print "total cost time:%s ms" % (cost_time)
 
 if __name__ == '__main__':
